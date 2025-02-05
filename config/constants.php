5289dbf1bc28607ccf67b4cadda43b4af120796c
Andy R <68365056+asriidev-gh@users.noreply.github.com>
2025-02-05 12:16:07 +0800

    clean up the code

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'github' => [
+        'repo_owner' => env('GITHUB_REPO_OWNER', 'asriidev-gh'),
+        'repo_name' => env('GITHUB_REPO_NAME', 'github-issues-app'),
+    ],
+];
